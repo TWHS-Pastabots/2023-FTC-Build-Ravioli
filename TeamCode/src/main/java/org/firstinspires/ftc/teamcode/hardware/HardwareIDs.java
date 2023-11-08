a11bf1eba17f4e7c74477a6aac7518086a3539a9
ComradeHibiscus <mark.vladimir.antonov@gmail.com>
2023-11-08 16:30:37 -0600

    updated ids

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/HardwareIDs.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/HardwareIDs.java
@@ -6,11 +6,8 @@ public class HardwareIDs {
     public static final String RIGHT_BACK_MOTOR = "rightRear";
     public static final String LEFT_BACK_MOTOR = "leftRear";
     public static final String INTAKE_MOTOR = "intakeMotor";
-    public static final String INTAKE_RAMP_MOTOR = "intakeRampMotor";
-    public static final String FLYWHEEL_1_MOTOR = "flywheel1Motor";
-    public static final String FLYWHEEL_2_MOTOR = "flywheel2Motor";
-    public static final String SCAFFOLD_SERVO_ONE = "scaffoldServoOne";
-    public static final String SCAFFOLD_SERVO_TWO = "scaffoldServoTwo";
+    public static final String LAUNCHER_MOTOR ="launcherMotor";
+    public static final String FLYWHEEL_MOTOR = "flywheelMotor";
     public static final String LAUNCHER_SERVO = "launcherServo";
     public static final String ARM_SERVO_1 = "armServo1";
     public static final String ARM_SERVO_2 = "armServo2";
