ad726dd4675d9aaf7916a2a3e7a6c56a0f37578a
ComradeHibiscus <mark.vladimir.antonov@gmail.com>
2023-12-02 14:45:20 -0600

    changed constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -46,7 +46,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.476; // in
     public static double GEAR_RATIO = 1.5; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 15.25; // in
+    public static double TRACK_WIDTH = 19.7; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -54,7 +54,7 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.0229 / rpmToVelocity(MAX_RPM);
+    public static double kV = 0.0229;
     public static double kA = 0;
     public static double kStatic = 0;
 
